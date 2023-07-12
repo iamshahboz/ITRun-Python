829726ee58fe2da7113bd9c0e2e8b7e225deadc6
unknown <sharafzodashahboz@gmail.com>
2023-07-12 22:36:51 +0500

    today's code added

---
+++ prog1.py
@@ -0,0 +1,17 @@
+# name, age = "William", 39
+
+
+# name = "William"
+# age = 39
+
+# name = input("Your name please: ")
+# birth_year = int(input("Your birth year please: "))
+# print("you are", name, "and your age is ", 2023-birth_year)
+
+
+print('My name is Tom and I\'m 40')
+
+
+
+
+
