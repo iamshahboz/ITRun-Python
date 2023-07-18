873834bfe8d32d7bcb6f79c1db299b4a3348ad1c
unknown <sharafzodashahboz@gmail.com>
2023-07-18 10:17:29 +0500

    code added

---
+++ prog2.py
@@ -0,0 +1,61 @@
+# number1 = 12
+# number2 = 8
+
+# number1, number2 = number2, number1
+
+# print(number1)
+# print(number2)
+
+# name = "Pikward"
+# age = 19
+
+# print(f"His name is {name} and he is {age}")
+
+# country = "Tajikistan"
+# l1 = country[0]
+# l_middle = country[2:]
+# l_end = country[-1]
+# print(l1, l_middle, l_end)
+
+# car_model = "Range Rover"
+# for letter in car_model:
+#     print(letter)
+
+#print("My name\n" * 5)
+#print("Toyota\t" * 4)
+
+#sentence = "I love programming"
+
+# word = "Gaming"
+# new_word = "Laming"
+# print(new_word)
+
+
+# word= "mouse"
+# print(word)
+# word = "Cat"
+
+name1 = "Tom"
+name2 = "Mike"
+name3 = "Elisa"
+
+if name1 == "Tom" or name3 == "Tom":
+    print("Yes, Tom is here")
+else:
+    print("No, Tom is not here")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
