4c98ba6c29b8cf9b85007a418c3d651941ecf101
unknown <sharafzodashahboz@gmail.com>
2023-07-19 20:00:04 +0500

    new lesson added

---
+++ prog3.py
@@ -0,0 +1,104 @@
+# a = input("Say something: ")
+# if not a:
+#     print("You should write something")
+# else:
+#     print(a)
+
+# countries = ["Albania","Estonia","Germany","Finland"]
+# countries[0] = 'Tajikistan'
+# print(countries)
+
+# for country in countries:
+#     print(country)
+
+# number = [8, 13, 4, 0, 22]* 10
+# print(number)
+
+# a = number[0]*10
+# print(a)
+
+
+# countries = ["Albania","Estonia","Germany","Finland"]
+# if not "Canada" in countries:
+#     print("Canada is not in the list ")
+# else:
+#     print("Canada is in the list")
+
+# numbers1 = [2,6,9]
+# numbers2 = [0,8,12]
+# result = numbers1 + numbers2
+# print(result)
+
+# numbers = [2,5,9,44]
+# print(sum(numbers))
+
+# numbers = [0, 12, 0, 8, 44, 9]
+# print(numbers[-1])
+# print(numbers[3:5])
+# print(sum(numbers))
+# new_numbers = [1]*5
+# print(new_numbers)
+# print(numbers.count(0))
+
+# numbers = [0, 12, 0, 8, 44, 9]
+# print(numbers.index(8))
+
+# digit = [2,0,70]
+# digit.append(10)
+# print(digit)
+# print(id(digit))
+
+# digit = [2,0,70,101,44,28,30]
+# digit.insert(2,3)
+# print(digit)
+
+# digit = [2,0,70,101,44,28,30]
+# digit.extend([2,0,0])
+# print(digit)
+
+# digits = [-2, 14, 33, 108, -1, -7, 1]
+# positive_digits = []
+# negative_digits = []
+# for number in digits:
+#     if number > 0:
+#         positive_digits.append(number)
+#     else:
+#         negative_digits.append(number)
+
+# print(positive_digits)
+# print(negative_digits)
+
+# letter = ('q')
+# letter2 = ('r',)
+# print(type(letter))
+# print(type(letter2))
+
+# numbers = [0, 12, 0, 8, 44, 9]
+# my_set = set(numbers)
+# print(my_set)
+
+# new_set = {3,21, 20, 88, 65}
+# new_set.add(-1)
+# print(new_set)
+
+country = {'Italy','Norway','Hungary'}
+country_new = {'Uzbekistan','Kazakhstan','Tajikistan'}
+all_countries = country.union(country_new)
+print(all_countries)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
