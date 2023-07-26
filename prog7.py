b9fea3825fbb6353e75bc5d4ba5b9086a20736dc
unknown <sharafzodashahboz@gmail.com>
2023-07-26 20:01:22 +0500

    next class added

---
+++ prog7.py
@@ -0,0 +1,35 @@
+
+# some more info about functions
+# def my_function(number1, number2:int):
+#     result = number1 + number2 + number4
+#     return result 
+
+
+# print(my_function(4.8,9.5))
+
+# print("Hello\n"*5)
+# for i in range(5):
+#     print("Hello")
+
+numbers = [3, -5, 8, 11, 0, 33, 12, 100]
+# numbers.sort()
+# print(numbers)
+# print("------")
+
+# print(sorted(numbers))
+
+#sorting with for loop
+# for i in range(len(numbers)):
+#     for j in range(len(numbers)-1):
+#         if numbers[i] < numbers[j]:
+#             numbers[i],numbers[j]= numbers[j],numbers[i]
+
+# print(numbers)
+
+number1 = 10
+number2 = 45
+number1,number2 = number2, number1
+print(number1)
+print(number2)
+
+
